15bec58a745a06256768a278d4d55f5935629f3a
Mateo <mroldantaborda1@gmail.com>
2025-10-07 16:49:13 -0500

    creation of repository and bolt install

---
+++ src/constants/app.constant.js
@@ -0,0 +1,2 @@
+export const APP_NAME = 'Bolt'
+export const REDIRECT_URL_KEY = 'redirectUrl'
---
+++ src/mock/data/authData.js
@@ -0,0 +1,11 @@
+export const signInUserData = [
+    {
+        id: '21',
+        avatar: '',
+        userName: 'John Doe',
+        email: 'admin-01@tatco.construction',
+        authority: ['admin', 'user'],
+        password: '123Qwe',
+        accountUserName: 'admin',
+    },
+]
